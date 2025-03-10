4615bab1fb028b28888a205f3614a5bc83d63088
Rohitbanala <o180361@rguktong.ac.in>
2025-03-10 12:37:01 +0530

    initial commit of cartwish

---
+++ src/components/Navbar/NavBar.jsx
@@ -0,0 +1,11 @@
+import "./NavBar.css";
+export default function NavBar() {
+  return (
+    <div className="navbar">
+      <div>
+        <h1 className="navbar_heading">CartWish</h1>
+      </div>
+      <div></div>
+    </div>
+  );
+}
