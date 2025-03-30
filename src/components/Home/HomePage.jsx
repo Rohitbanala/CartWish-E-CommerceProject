6196a0891dcd2b8353c2f33152df532e0f2f3371
Rohitbanala <o180361@rguktong.ac.in>
2025-03-30 18:04:40 +0530

    fixed links in hero sctions

--- src/components/Home/HomePage.jsx
+++ src/components/Home/HomePage.jsx
@@ -8,14 +8,14 @@ export default function HomePage() {
       <HeroSection
         title="Buy iPhone 14 pro"
         subtitle="Experience the power of the latest iPhone 14 with our most Pro camera ever."
-        link="/product/67d121663b9fc8f17b4dbc45"
+        link="product/67ddc1c1466447fbc2d8a283"
         image={iPhone}
       />
       <FeaturedProducts />
       <HeroSection
         title="Build the ultimate setup"
         subtitle="You can add Studio Display and colour-matched Magic accessories to your bag after configure your Mac mini"
-        link="/product/67d121663b9fc8f17b4dbc45"
+        link="product/67ddc1c1466447fbc2d8a28b"
         image={mac}
       />
     </div>
