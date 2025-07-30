bcd145ce1e8bb1bf6b6f5f123088ae307b4ae9a7
Rohitbanala <o180361@rguktong.ac.in>
2025-07-30 22:14:29 +0530

    added image

--- src/components/Home/HomePage.jsx
+++ src/components/Home/HomePage.jsx
@@ -1,5 +1,5 @@
 import HeroSection from "./HeroSection";
-
+import iPhoneImage from "../../assets/iPhone-14-pro.webp";
 import mac from "../../assets/mac-system-cut.jfif";
 import FeaturedProducts from "./FeatureProducts";
 export default function HomePage() {
@@ -9,7 +9,7 @@ export default function HomePage() {
         title="Buy iPhone 14 pro"
         subtitle="Experience the power of the latest iPhone 14 with our most Pro camera ever."
         link="product/67ddc1c1466447fbc2d8a283"
-        image={mac}
+        image={iPhoneImage}
       />
       <FeaturedProducts />
       <HeroSection
